96bdaf56f69833c0f5aecbdde49d8b198465356e
AyomideKayode <ayomidekay7@gmail.com>
2024-05-16 01:25:16 +0100

    Log Formatter.🚧

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -9,6 +9,7 @@ Example usage:
     logger.info("User logged in: name=john, email=john@example.com")
 """
 
+import logging
 import re
 from typing import List
 
@@ -35,3 +36,38 @@ def filter_datum(fields: List[str],
     filtered_data = re.sub(
         pattern, lambda m: f"{m.group().split('=')[0]}={redaction}", message)
     return filtered_data
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+    This class will redact sensitive data from log messages.
+    """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """ Initialize the RedactingFormatter object
+        Arguments:
+          fields -- list of strings representing fields to obfuscate
+        """
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        # Initialize the fields attribute with the provided list of fields
+        self.fields = fields if fields is not None else []
+
+    def format(self, record: logging.LogRecord) -> str:
+        """ Format the log record
+        By first calling the format() method of the parent class, we can
+        obtain the original log message.
+        We then call the filter_datum() function to obfuscate the sensitive
+        data in the log message.
+        Arguments:
+          record -- log record to format
+        Returns:
+        The formatted log message.
+        """
+        original_message = super(RedactingFormatter, self).format(record)
+        filtered_data = filter_datum(
+            self.fields, self.REDACTION, original_message, self.SEPARATOR)
+        return filtered_data

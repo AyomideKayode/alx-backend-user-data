28789662a47b18105746595524dbf115f67a61f7
AyomideKayode <ayomidekay7@gmail.com>
2024-05-24 23:52:53 +0100

    Add the URL path to list of excluded_paths.🚧

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -63,7 +63,7 @@ def before_request() -> str:
     if auth is None:
         return
     excluded_paths = ['/api/v1/status/', '/api/v1/unauthorized/',
-                      '/api/v1/forbidden/']
+                      '/api/v1/forbidden/', '/api/v1/auth_session/login/']
     if not auth.require_auth(request.path, excluded_paths):
         return
     if auth.authorization_header(request) is None and auth.session_cookie(

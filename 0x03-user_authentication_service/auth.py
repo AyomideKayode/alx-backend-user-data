cb2e82462aed49e9d41109c28809b0b865906c37
AyomideKayode <ayomidekay7@gmail.com>
2024-05-28 14:28:49 +0100

    Define register_user method and correctly handle user registration, check for existing users and raise exceptions where necessary.🚧

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -3,8 +3,10 @@
 """ User authentication service module
 """
 
-from db import DB
 import bcrypt
+from db import DB
+from user import User
+from sqlalchemy.orm.exc import NoResultFound
 
 
 def _hash_password(password: str) -> bytes:
@@ -20,3 +22,32 @@ def _hash_password(password: str) -> bytes:
     hashed_password = bcrypt.hashpw(password.encode(), salt)
 
     return hashed_password
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """ Register a new user by hashing the password and adding the user
+        to the database.
+        Args:
+            email (str): The email of the new user.
+            password (str): The password of the new user
+        Returns:
+            User: The newly created User object.
+        Raises:
+            ValueError: If a user with the given email already exists.
+        """
+        try:
+            self._db.find_user_by(email=email)
+            raise ValueError(f"User {email} already exists")
+        except NoResultFound:
+            # user doesn't exist, we proceed to create new user
+            hashed_password = _hash_password(password)
+            new_user = self._db.add_user(email, hashed_password)
+
+            return new_user

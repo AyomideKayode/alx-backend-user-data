b4de3247e5e3e642a99a958755fea1aede15eb7a
AyomideKayode <ayomidekay7@gmail.com>
2024-05-28 17:52:08 +0100

    Create /users endpoint to handle user registration and duplicate registrations correctly.🚧

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -3,9 +3,11 @@
 """ Basic Flask App setup.
 """
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
 
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route("/", methods=["GET"], strict_slashes=False)
@@ -16,6 +18,27 @@ def welcome():
         (str): JSON payload response
     """
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route("/users", methods=["POST"], strict_slashes=False)
+def users():
+    """ POST '/users' route that registers a new user.
+    Returns:
+        Response: JSON response indicating success if user was fully registered
+                    or error if user already exists.
+    """
+    email = request.form.get("email")
+    password = request.form.get("password")
+
+    if not email or not password:
+        return jsonify({"message": "email and password required"}), 400
+
+    try:
+        new_user = AUTH.register_user(email, password)
+        return jsonify({"email": new_user.email,
+                        "message": "user created"}), 200
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
